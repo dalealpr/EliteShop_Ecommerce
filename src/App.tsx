2e2329bfd5e6553cdcd16be7591ce6f795d6ffb5
David Leal <daleal.prgmr@gmail.com>
2024-09-10 19:45:30 -0300

    start: comenzando proyecto

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+import "./App.css";
+import HomePage from "./pages/HomePage/HomePage";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div>
+      <HomePage />
+    </div>
+  );
+}
+
+export default App;
